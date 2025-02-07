05d917fb651f617c7b82b6652ebfff3480aa793a
Sebastyk120 <90669455+Sebastyk120@users.noreply.github.com>
2025-02-07 12:25:35 +0100

    Update models2.py

--- comercial/models2.py
+++ comercial/models2.py
@@ -1,10 +1,16 @@
 from django.core.validators import MinValueValidator
 from django.db import models
-from choices import origen_nacional
+from .choices import origen_nacional
 from comercial.models import Fruta, Exportador
 
 
 # Create your models here.
+
+class ProveedorNacional(models.Model):
+    nombre = models.CharField(max_length=50, verbose_name="Nombre Proveedor", unique=True)
+    telefono = models.CharField(max_length=20, verbose_name="Telefono Proveedor", blank=True, null=True)
+    email = models.EmailField(max_length=50, verbose_name="Email Proveedor", blank=True, null=True)
+    observaciones = models.TextField(verbose_name="Observaciones", blank=True, null=True)
 
 class Empaque(models.Model):
     nombre = models.CharField(max_length=20, verbose_name="Nombre Empaque", unique=True)
@@ -12,6 +18,7 @@ class Empaque(models.Model):
 
 
 class CompraNacional(models.Model):
+    proveedor = models.ForeignKey(ProveedorNacional, on_delete=models.PROTECT, verbose_name="Proveedor")
     origen_compra = models.CharField(max_length=20 , choices=origen_nacional, verbose_name="Origen")
     fruta = models.ForeignKey(Fruta, on_delete=models.PROTECT, verbose_name="Fruta")
     peso_compra = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Peso Guia", default=0, validators=[MinValueValidator(0.0)])
@@ -20,8 +27,8 @@ class CompraNacional(models.Model):
     numero_guia = models.CharField(max_length=20, verbose_name="Numero Guia", unique=True, blank=True, null=True)
     remision = models.CharField(max_length=20, verbose_name="Remision", blank=True, null=True, default='Sin Remision')
     peso_remision = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Peso Remision", blank=True, null=True, validators=[MinValueValidator(0.0)], default=0)
-    precio_compra_exportacion = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="$ Compra Exportacion", validators=[MinValueValidator(0.0)])
-    precio_compra_nacional = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="$ Compra Nacional", blank=True, null=True, validators=[MinValueValidator(0.0)])
+    precio_compra_exp = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="$ Compra Exportacion", validators=[MinValueValidator(0.0)])
+    precio_compra_nal = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="$ Compra Nacional", blank=True, null=True, validators=[MinValueValidator(0.0)])
     tipo_empaque = models.ForeignKey(Empaque, on_delete=models.PROTECT, verbose_name="Tipo Empaque")
     cantidad_empaque = models.PositiveIntegerField(verbose_name="Cantidad Empaque", validators=[MinValueValidator(0)])
     observaciones = models.TextField(verbose_name="Observaciones", blank=True, null=True)
@@ -29,13 +36,13 @@ class CompraNacional(models.Model):
 class VentaNacional(models.Model):
     compra_nacional = models.OneToOneField(CompraNacional, on_delete=models.CASCADE, verbose_name="Compra Nacional", primary_key=True)
     exportador = models.ForeignKey(Exportador, on_delete=models.PROTECT, verbose_name="Exportador")
-    cantidad_empaque_vendida = models.PositiveIntegerField(verbose_name="Cantidad Empaque Vendida", validators=[MinValueValidator(0)], blank=True, null=True)
+    cantidad_empaque_enviada = models.PositiveIntegerField(verbose_name="Cantidad Empaque Enviada", validators=[MinValueValidator(0)], blank=True, null=True)
     peso_bruto_recibido = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Peso Bruto Recibido", validators=[MinValueValidator(0.0)], blank=True, null=True)
     peso_neto_recibido = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Peso Neto Recibido", validators=[MinValueValidator(0.0)], blank=True, null=True, editable=False)
 
     def save(self, *args, **kwargs):
-        if not self.cantidad_empaque_vendida:  # Si no se ha modificado, ingreso la cantidad de empaque de compra.
-            self.cantidad_empaque_vendida = self.compra_nacional.cantidad_empaque
+        if not self.cantidad_empaque_enviada:  # Si no se ha modificado, ingreso la cantidad de empaque de compra.
+            self.cantidad_empaque_enviada = self.compra_nacional.cantidad_empaque
         super().save(*args, **kwargs)
 
 class ReporteCalidadExportador(models.Model):
